82fd212e973ba18eed08614a4dc5cd84ee91d617
Dicky03a <dickycoding@gmail.com>
2025-10-26 23:53:33 +0700

    first commit

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title', 'Dashboard Admin')
+
+@section('content')
+<div class="p-4 mx-auto max-w-screen-2xl md:p-6">
+      <div class="grid grid-cols-12 gap-4 md:gap-6">
+            <div class="col-span-12 space-y-6 xl:col-span-7">
+                  <h1 class="text-3xl font-bold text-gray-800 dark:text-gray-100">Selamat Datang</h1>
+                  <p class="text-gray-500 dark:text-gray-400">Ini halaman dashboard Laravel dengan template TailAdmin 🚀</p>
+            </div>
+      </div>
+</div>
+@endsection
